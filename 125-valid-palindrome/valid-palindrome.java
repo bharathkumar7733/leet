6c7f2305322bafb7971a4dfd6fc495ddbdd5ab66
bharathkumar7733 <chappabharathkumar8@gmail.com>
2025-12-09 21:48:25 +0530

    Time: 4 ms (51.88%) | Memory: 44.8 MB (45.83%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Convert to lowercase and remove non-alphanumeric characters
+        StringBuilder cleaned = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+        
+        // Two-pointer check
+        int left = 0, right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
